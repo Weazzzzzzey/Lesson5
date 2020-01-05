0f448d327de75b29632eb7f69f0ecae8494b324f
Weazzzzzzey <heronightmares@gmail.com>
2020-01-06 01:03:12 +0200

    Prideta pirma programa is namu darbu

---
+++ VardasIrData/VardasIrData/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace VardasIrData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ivedimas = new System.Windows.Forms.Button();
+            this.OutPut = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Ivedimas
+            // 
+            this.Ivedimas.Location = new System.Drawing.Point(12, 93);
+            this.Ivedimas.Name = "Ivedimas";
+            this.Ivedimas.Size = new System.Drawing.Size(184, 58);
+            this.Ivedimas.TabIndex = 0;
+            this.Ivedimas.Text = "Ivedimas";
+            this.Ivedimas.UseVisualStyleBackColor = true;
+            this.Ivedimas.Click += new System.EventHandler(this.Ivedimas_Click);
+            // 
+            // OutPut
+            // 
+            this.OutPut.Location = new System.Drawing.Point(13, 13);
+            this.OutPut.Multiline = true;
+            this.OutPut.Name = "OutPut";
+            this.OutPut.Size = new System.Drawing.Size(183, 74);
+            this.OutPut.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(208, 163);
+            this.Controls.Add(this.OutPut);
+            this.Controls.Add(this.Ivedimas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Ivedimas;
+        private System.Windows.Forms.TextBox OutPut;
+    }
+}
+
---
+++ VardasIrData/VardasIrData/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VardasIrData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Ivedimas.Name = "Taip?";
+        }
+
+        private void Ivedimas_Click(object sender, EventArgs e)
+        {
+            DateTime Dabar = DateTime.Now;
+            string now = Dabar.ToString("\r\ndd MMMM yyyy");
+            OutPut.Text = "Sveikas Programuotojau";
+            OutPut.AppendText(now);
+
+
+        }
+    }
+}
